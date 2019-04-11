57bb2d80390031cdc495a3f9bf15abe455fa6369
Oxnock <jsezuniga72@gmail.com>
2019-04-11 15:38:29 -0600

    Initial Commit
    :vvvvvvv

---
+++ Tarea4Topicos/Tarea4Topicos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tarea4Topicos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
